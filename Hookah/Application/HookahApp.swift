eb5f56bf6d78652441a401f6e016a7fcf751a3ed
OguzhanCinar-01 <118293608+OguzhanCinar-01@users.noreply.github.com>
2025-12-01 00:49:46 +0300

    chore: organize project folder structure

---
+++ Hookah/Application/HookahApp.swift
@@ -0,0 +1,17 @@
+//
+//  HookahApp.swift
+//  Hookah
+//
+//  Created by Oğuzhan Cnr on 1.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct HookahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
